c830233543087e55cd66d3bd9b92083afe0bed66
KarimSalem9000 <52518913+KarimSalem9000@users.noreply.github.com>
2023-11-10 16:27:36 -0500

    Update wisp.py

--- wisp.py
+++ wisp.py
@@ -1,21 +1,39 @@
 import whisper
 
-model = whisper.load_model("base")
+#model = whisper.load_model('base')
+#result = model.transcribe('audio.mp3')
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("pink.mp3")
-audio = whisper.pad_or_trim(audio)
+#print(result['text'])
+#----------------
+import subprocess
+import openai
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+# Function to transcribe audio to text using OpenAI Whisper API
+def transcribe_audio_to_text(x):
+    model = whisper.load_model('base')
+    result = model.transcribe(x)
 
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+    return result['text']
 
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
+# Main function
+def main():
+    # Define the path for the audio file and output text file
+    video_path = 'kamala.mp4'  # Replace with the path to your video file
+    audio_path = 'audio.mp3'
+    text_output_path = 'transcription.txt'
 
-# print the recognized text
-print(result.text)
+    # Extract audio from the video
+    cmd = ['ffmpeg', '-i', video_path, '-vn', '-ar', '44100', '-ac', '1', '-b:a', '32k', '-f', 'mp3', audio_path]
+    subprocess.call(cmd)
+
+    # Transcribe the extracted audio to text
+    transcription = transcribe_audio_to_text(audio_path)
+
+    # Save the transcription to a text file
+    with open(text_output_path, 'w') as text_file:
+        text_file.write(transcription)
+
+    print(f"Transcription saved to {text_output_path}")
+
+if __name__ == "__main__":
+    main()
