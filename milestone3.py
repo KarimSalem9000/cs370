62214a4319c1b08926f87477b859dc7775142166
KarimSalem9000 <52518913+KarimSalem9000@users.noreply.github.com>
2023-11-27 17:45:01 -0500

    milestone3.py

--- milestone3.py
+++ milestone3.py
@@ -1,35 +1,28 @@
+import gradio as gr
+from transformers import pipeline
 
+# Instantiate translation pipeline
+tran_pip = pipeline("translation_en_to_de")
 
-import subprocess
-import whisper
-import openai
+# Example translation with max_length
+result = tran_pip("I like icecream", max_length=512)
+print(result)
 
-# Function to transcribe audio to text using OpenAI Whisper API
-def transcribe_audio_to_text(x):
-    model = whisper.load_model('base')
-    result = model.transcribe(x)
+ccfile = open("transcription.txt", "r")
+f = open("translation.txt", "a")
 
-    return result['text']
+stuff = []
+for aline in ccfile:
+    stuff = aline.split()
+fin = ""
+for i in stuff:
+    result = tran_pip(i, max_length=512)
+    fin += " " + result[0]['translation_text']
+    
 
-# Main function
-def main():
-    # Define the path for the audio file and output text file
-    video_path = 'kamala.mp4'  # Replace with the path to your video file
-    audio_path = 'audio.mp3'
-    text_output_path = 'transcription.txt'
-
-    # Extract audio from the video
-    cmd = ['ffmpeg', '-i', video_path, '-vn', '-ar', '44100', '-ac', '1', '-b:a', '32k', '-f', 'mp3', audio_path]
-    subprocess.call(cmd)
-
-    # Transcribe the extracted audio to text
-    transcription = transcribe_audio_to_text(audio_path)
-
-    # Save the transcription to a text file
-    with open(text_output_path, 'w') as text_file:
-        text_file.write(transcription)
-
-    print(f"Transcription saved to {text_output_path}")
+f.write(f)
+print(fin)  
+ccfile.close()
 
 if __name__ == "__main__":
     main()
